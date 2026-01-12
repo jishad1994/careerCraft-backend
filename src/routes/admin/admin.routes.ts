da9c8a4be4e4bccee577b7581604d6265be5cd05
jishad kolapurath <jishadkolapurath@gmail.com>
2026-01-13 02:00:48 +0530

    refactor(controller): made some line changes

--- src/routes/admin/admin.routes.ts
+++ src/routes/admin/admin.routes.ts
@@ -7,8 +7,8 @@ export const adminRoutes = express.Router();
 
 adminRoutes.get("/getCompanies", adminController.getCompanies.bind(adminController));
 adminRoutes.get("/getUsers", adminController.getUsers.bind(adminController));
-adminRoutes.patch("/comapanies/:id/block", adminController.blockCompany.bind(adminController));
-adminRoutes.patch("/comapanies/:id/unblock", adminController.unblockCompany.bind(adminController));
+adminRoutes.patch("/companies/:id/block", adminController.blockCompany.bind(adminController));
+adminRoutes.patch("/companies/:id/unblock", adminController.unblockCompany.bind(adminController));
 adminRoutes.patch("/users/:id/block", adminController.blockUser.bind(adminController));
 adminRoutes.patch("/users/:id/unblock", adminController.unblockUser.bind(adminController));
 
