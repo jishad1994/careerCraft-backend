78013816058e5d428fe726b9be5fc4a4f9d503e3
jishad kolapurath <jishadkolapurath@gmail.com>
2026-05-07 02:51:17 +0530

    change cookie option same site to none for production environment

--- src/utils/cookies.utils.ts
+++ src/utils/cookies.utils.ts
@@ -1,21 +1,22 @@
 import { CookieOptions } from "express";
 
 export const refreshTokenCookieName = "refreshToken";
+export const accessTokenCookieName = "accessToken";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const refreshTokenCookieOptions: CookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", //only true in production
-    sameSite: "strict",
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
     path: "/",
-    maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
+    maxAge: 7 * 24 * 60 * 60 * 1000,
 };
-
-export const accessTokenCookieName = "accessToken";
 
 export const accessTokenCookieOptions: CookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", //only true in production
-    sameSite: "strict",
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
     path: "/",
-    maxAge: 15 * 60 * 1000, //15 minutes
+    maxAge: 15 * 60 * 1000,
 };
